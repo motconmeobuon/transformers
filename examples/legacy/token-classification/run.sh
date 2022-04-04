cbb12b885f6f9e3cbf67d91c23de521b2010a679
Nguyễn Thành Đức <37094982+motconmeobuon@users.noreply.github.com>
2022-04-04 16:22:41 +0700

    Update run.sh

--- examples/legacy/token-classification/run.sh
+++ examples/legacy/token-classification/run.sh
@@ -1,23 +1,22 @@
 ## The relevant files are currently on a shared Google
 ## drive at https://drive.google.com/drive/folders/1kC0I2UGl2ltrluI9NqDjaQJGw5iliw_J
 ## Monitor for changes and eventually migrate to nlp dataset
-curl -L 'https://drive.google.com/uc?export=download&id=1Jjhbal535VVz2ap4v4r_rN1UEHTdLK5P' \
-| grep -v "^#" | cut -f 2,3 | tr '\t' ' ' > train.txt.tmp
-curl -L 'https://drive.google.com/uc?export=download&id=1ZfRcQThdtAR5PPRjIDtrVP7BtXSCUBbm' \
-| grep -v "^#" | cut -f 2,3 | tr '\t' ' ' > dev.txt.tmp
-curl -L 'https://drive.google.com/uc?export=download&id=1u9mb7kNJHWQCWyweMDRMuTFoOHOfeBTH' \
-| grep -v "^#" | cut -f 2,3 | tr '\t' ' ' > test.txt.tmp
+mkdir -p villa_ner
 
-export MAX_LENGTH=128
-export BERT_MODEL=bert-base-multilingual-cased
-python3 scripts/preprocess.py train.txt.tmp $BERT_MODEL $MAX_LENGTH > train.txt
-python3 scripts/preprocess.py dev.txt.tmp $BERT_MODEL $MAX_LENGTH > dev.txt
-python3 scripts/preprocess.py test.txt.tmp $BERT_MODEL $MAX_LENGTH > test.txt
+curl -L 'https://raw.githubusercontent.com/motconmeobuon/villaaaa/master/train.conll'  | tr '\t' ' ' > villa_ner/train.txt.tmp
+curl -L 'https://raw.githubusercontent.com/motconmeobuon/villaaaa/master/val.conll' | tr '\t' ' ' > villa_ner/dev.txt.tmp
+curl -L 'https://raw.githubusercontent.com/motconmeobuon/villaaaa/master/test.conll' | tr '\t' ' ' > villa_ner/test.txt.tmp
+
+export MAX_LENGTH=64
+export BERT_MODEL=vinai/phobert-base
+python3 scripts/preprocess.py villa_ner/train.txt.tmp $BERT_MODEL $MAX_LENGTH > train.txt
+python3 scripts/preprocess.py villa_ner/dev.txt.tmp $BERT_MODEL $MAX_LENGTH > dev.txt
+python3 scripts/preprocess.py villa_ner/test.txt.tmp $BERT_MODEL $MAX_LENGTH > test.txt
 cat train.txt dev.txt test.txt | cut -d " " -f 2 | grep -v "^$"| sort | uniq > labels.txt
 export OUTPUT_DIR=germeval-model
-export BATCH_SIZE=32
-export NUM_EPOCHS=3
-export SAVE_STEPS=750
+export BATCH_SIZE=16
+export NUM_EPOCHS=75
+export SAVE_STEPS=20000
 export SEED=1
 
 python3 run_ner.py \
